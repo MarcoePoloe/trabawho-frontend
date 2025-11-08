ff836d09130124322bede138ffc5a255893cf88b
unknown <sillydudee@gmail.com>
2025-11-08 14:04:42 +0800

    Redef cloudflared

--- constants/apiurl.js
+++ constants/apiurl.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://tiles-layout-student-oxide.trycloudflare.com';
+export const API_URL = 'https://machine-focal-views-spirits.trycloudflare.com';
 // Production URL
 // export const API_URL = 'https://trabawho-backend.onrender.com';
